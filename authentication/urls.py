628c8c0037c612fef44e28b20c574a29e5654e42
Tran Hai <khoidaumoichotoi@gmail.com>
2022-12-30 11:52:33 +0700

    username ajax validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 from authentication.views import *
 
 app_name = 'authentication'
 urlpatterns = [
     path('register/', RegistrationView.as_view() , name='register'),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()) , name='validate-username'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+import json
+
 from django.views import generic
+from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+class UsernameValidationView(generic.View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            username = data['username']
+            if not str(username).isalnum():
+                return JsonResponse(data={
+                    'username_error' : 'Username should only contain alpha numeric characters'
+                })
+            return JsonResponse(data={'Username is valid' : True})
+        except Exception as ex:
+            print('USER NAME VALIDATION POST REQUEST ERROR: ', ex)
+
 class RegistrationView(generic.View):
     template_name = 'authentication/register.html'
 

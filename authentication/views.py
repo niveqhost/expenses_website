6fc2dbcb3398a89213344ad8a999b35f8ffa8970
Tran Hai <khoidaumoichotoi@gmail.com>
2022-12-30 17:29:58 +0700

    ajax validate username

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ import json
 from django.views import generic
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 class UsernameValidationView(generic.View):
@@ -13,8 +14,12 @@ class UsernameValidationView(generic.View):
             if not str(username).isalnum():
                 return JsonResponse(data={
                     'username_error' : 'Username should only contain alpha numeric characters'
-                })
-            return JsonResponse(data={'Username is valid' : True})
+                }, status=400)
+            if User.objects.filter(username=username).exists():
+                return JsonResponse(data={
+                    'username_error' : 'This username is already taken. Please choose another name.'
+                }, status=409)
+            return JsonResponse(data={'Username is valid' : True}, status=200)
         except Exception as ex:
             print('USER NAME VALIDATION POST REQUEST ERROR: ', ex)
 
---
+++ static/js/register.js
@@ -0,0 +1,27 @@
+// @ts-nocheck
+document.addEventListener("DOMContentLoaded", function () {
+    const username_field = document.querySelector("#username-field");
+    const feedback_area = document.querySelector(".invalid-feedback");
+
+    username_field.addEventListener("keyup", function (event) {
+        let username = event.target.value;
+        username_field.classList.remove("is-invalid");
+        feedback_area.style.display = "none";
+        if (username.length > 0) {
+            fetch("/account/validate-username", {
+                body: JSON.stringify({ username: username }),
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }).then(function(response) { return response.json(); })
+                .then(function (data) {
+                    console.log(data.username_error);
+                username_field.classList.add("is-invalid");
+                feedback_area.style.display = "block";
+                feedback_area.innerHTML = `<p>${data.username_error}</p>`;
+            })
+            .catch(function(error) { console.error("Error:", error) });
+        }
+    });
+});

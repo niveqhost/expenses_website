e8ebc84480162f1701364272f3f0907a27f17029
Tran Hai <khoidaumoichotoi@gmail.com>
2023-01-01 11:27:12 +0700

    more

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,6 +1,5 @@
 from core.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-63d1.up.railway.app']

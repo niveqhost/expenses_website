457e84a85aa8e82a1bea6ef0e9bc66555d0d5d76
Tran Hai <khoidaumoichotoi@gmail.com>
2023-01-01 11:38:26 +0700

    more

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn core.wsgi --log-file -
+web: gunicorn core.wsgi --log-file - && python manage.py runserver --settings core.settings.production
--- core/settings/local.py
+++ core/settings/local.py
@@ -1,9 +1,9 @@
 from core.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    '*'
 ]

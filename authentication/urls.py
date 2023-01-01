421b50da792176533d7761fdfbdf15b435bc164a
Tran Hai <khoidaumoichotoi@gmail.com>
2023-01-01 21:04:46 +0700

    update authentication/urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 from authentication.views import *
 
 app_name = 'authentication'
 urlpatterns = [
-    path('register/', RegistrationView.as_view() , name='register'),
+    path('register/', csrf_protect(RegistrationView.as_view()) , name='register'),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()) , name='validate-username'),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()) , name='validate-email'),
 ]

994304211a39229e207de594e7c3c39f32f7a735
Tran Hai <khoidaumoichotoi@gmail.com>
2023-01-01 21:12:09 +0700

    update csrf trusted origin in settings/production.py

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,6 +1,10 @@
 from core.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hai-expenses.up.railway.app']
+
+# Origin checking failed - https://hai-expenses.up.railway.app does not match any trusted origins.
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['hai-expenses.up.railway.app']
